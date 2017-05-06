62db1ca3289ca8c982c25be4419f53e61ed58cfe
ahillegass <ahillegass@miami.edu>
2017-05-06 17:44:28 -0400

    changed devise layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  layout :layout_by_resource
+  layout "web/layouts/application"
  
-  private
-
-  def layout_by_resource
-    if devise_controller?
-      "web/layouts/application"
-    else
-      "web/layouts/application"
-    end
-  end
 end
